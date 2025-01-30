f4a0f0a8ee27ba22920d8206c4398c52b8c69277
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2025-01-30 14:56:31 +0500

    adds config

---
+++ internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Aggregations []Aggregation `yaml:"aggregations"`
+}
+
+type Aggregation struct {
+	Path     string  `yaml:"path"`
+	Calls    []Call  `yaml:"calls"`
+	Response Mapping `yaml:"response"`
+}
+
+type Call struct {
+	Name     string `yaml:"name"`
+	Backend  string `yaml:"backend"`
+	Required bool   `yaml:"required,omitempty"`
+}
+
+type Mapping struct {
+	Structure map[string]string `yaml:"structure"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Error loading configuration:", err)
+		return nil, err
+	}
+	var cfg Config
+	err = yaml.Unmarshal(file, &cfg)
+	if err != nil {
+		log.Fatal("YAML parsing error:", err)
+		return nil, err
+	}
+	log.Println("Configuration loaded successfully")
+	return &cfg, nil
+}
